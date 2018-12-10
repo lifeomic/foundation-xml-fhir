83504fc5bd3a4e97a3134bebcbc7ca8eb19fd152
Mark Schroering <mark.schroering@lifeomic.com>
2018-12-10 07:35:56 -0500

    Handle complex frameshift deletions

--- src/utils.py
+++ src/utils.py
@@ -41,6 +41,10 @@ def parse_splice(cdsEffect, position, strand, fasta):
         mylist = cdsEffect.split('>')
         mylist[0] = re.sub(r'[A-Za-z]','',mylist[0])
         mytype = 'sub'
+    elif '>' in cdsEffect:
+        mylist = cdsEffect.split('>')
+        mylist[0] = re.sub(r'[A-Za-z]','',mylist[0])
+        mytype = 'sub'
     elif 'delins' in cdsEffect:
         mylist = cdsEffect.split('delins')
         mytype = 'delins'
@@ -50,40 +54,54 @@ def parse_splice(cdsEffect, position, strand, fasta):
     elif 'ins' in cdsEffect:
         mylist = cdsEffect.split('ins')
         mytype = 'ins'
+    elif 'dup' in cdsEffect:
+        mylist = cdsEffect.split('dup')
+        mytype = 'dup'
     else:
-        print ('ERROR: not sure how to interpret [{}]'.format(cdsEffect))
-        raise ValueError('ERROR: not sure how to interpret [{}]'.format(cdsEffect))
+        raise ValueError('ERROR: not sure how to interpret [{}]')
 
-    if strand == '-':
+    if strand == '-' and re.match(r'^[A-Za-z]+', mylist[1]):
         mylist[1] = getRevComp(mylist[1])
     myrange = mylist[0].split('_')
     if len(myrange) == 1:
-        ## single value not a range
-        if mytype != 'sub':
-            print ('ERROR: not a range value and not a substitution [{}]'.format(cdsEffect))
-            raise ValueError('ERROR: not a range value and not a substitution [{}]'.format(cdsEffect))
-        ref=getSequence(genome, chr, startPos, startPos)
-        return (chr, startPos, ref, mylist[1])
+        if mytype == 'sub':
+            ref = getSequence(genome, chr, startPos, startPos)
+            return (chr, startPos, ref, mylist[1])
+        elif mytype == 'del':
+            ref = getSequence(genome, chr, startPos, startPos+1)
+            return (chr, startPos, ref, ref[0])
+        elif mytype == 'dup':
+            ref = getSequence(genome, chr, startPos, startPos+1)
+            return (chr, startPos, ref[0], ref)
+        else:
+            raise ValueError('ERROR: not a range value and not a substitution or deletion [{}]'.format(cdsEffect))
     elif len(myrange) == 2:
         main = ['', '']
         sub = ['', '']
         for i in range(len(myrange)):
             if '+' in myrange[i]:
                 [main[i], sub[i]] = re.split('\+',myrange[i])
+            elif myrange[i].startswith('-'):
+                main[i] = myrange[i]
+                sub[i] = '0'
             elif '-' in myrange[i]:
                 [main[i], sub[i]] = re.split('-',myrange[i])
                 sub[i] = '-' + sub[i]
             else:
                 main[i] = myrange[i]
                 sub[i] = '0'
-        if main[0] == main[1]:
-            mylen = abs(int(sub[0]) - int(sub[1]))+1
-        elif mytype == 'del':
-            mylen = int(main[1]) - int(main[0]) + 1
 
+        if re.match(r'^[0-9]+', mylist[1]):
+            seqlen = int(mylist[1])
+        else:
+            seqlen = len(mylist[1])
+
+        if main[0].startswith('*') or main[1].startswith('*'):
+            mylen = seqlen
+        elif main[0] == main[1]:
+            mylen = abs(int(sub[0]) - int(sub[1]))+1
         else:
             if sub[0] != '0' and sub[1] != '0':
-                print ('ERROR: main position has to match for both intronic else one of them must be an exon: ')
                 raise ValueError('ERROR: main position has to match for both intronic else one of them must be an exon: ')
             elif sub[0] == '0':
                 mylen = abs(int(main[0]) - int(main[1]))+1
@@ -94,19 +112,30 @@ def parse_splice(cdsEffect, position, strand, fasta):
 
         if mytype == 'ins':
             if mylen != 2:
-                print ('ERROR: insertion but range is not 1 [{}]'.format(cdsEffect))
                 raise ValueError('ERROR: insertion but range is not 1 [{}]'.format(cdsEffect))
+            elif not re.match(r'^[A-Za-z]+$', mylist[1]):
+                raise ValueError('ERROR: insertion but no actual nucleotide sequence given {}'.format(cdsEffect))
             else:
                 ref = getSequence(genome, chr, startPos, startPos)
                 mylist[1] = ref + mylist[1]
         elif mytype == 'del':
         #    startPos = startPos - 1
+            if mylen != seqlen:
+                raise ValueError('ERROR: length of cds range does not match the given deleted sequence  [{}]'.format(cdsEffect))
             ref = getSequence(genome, chr, startPos, startPos+mylen)
             mylist[1] = ref[0]
+        elif mytype == 'dup':
+            if mylen != seqlen:
+                raise ValueError('ERROR: length of cds range does not match the given duplicated sequence  [{}]'.format(cdsEffect))
+            ref = getSequence(genome, chr, startPos, startPos+mylen)
+            mylist[1] = ref
+            ref = ref[0]
+        elif main[0].startswith('*') or main[1].startswith('*'):
+            raise ValueError('ERROR: insert+delete on 5UTR, unable to resolve sequence [{}]'.format(cdsEffect))
         else:
             ref = getSequence(genome, chr, startPos, startPos+mylen-1)
+
         return (chr, startPos, ref, mylist[1])
     else:
-        print ('ERROR: syntax dont fit')
         raise ValueError('ERROR: syntax dont fit')
 
